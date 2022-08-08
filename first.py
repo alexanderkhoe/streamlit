07fc0d1d177d584aa60cfa2a4ecddc9457e9b265
alexanderkhoe <alexkhoe@hotmail.com>
2022-08-08 20:14:16 +0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Title
+st.title("Judul Website") 
+# Header
+st.header("Header Website")
+# Sub Header
+st.subheader("Sub Header Website")
+# Text
+st.markdown('My First Website') 
+st.write("My First Website")
+st.write("My First Website", 123)
+st.text("My First Website")
+st.code("My First Website")
+# success
+st.success('My First success') 
+# error
+st.error('My First error') 
+# warning
+st.warning('My First warning') 
+# info
+st.info('My First info') 
+# exception
+st.exception('My First info') 
+# help
+st.help(range) 
+
